dc1e7cab2695fed8c65eb1a296b88ce7a6f44bf8
Anseeuw Florian <florian.anseeuw@gmail.com>
2023-07-26 11:48:23 +0200

    create Views

---
+++ Views/layout.php
@@ -0,0 +1,20 @@
+<?php
+require_once('header.php');
+?>
+
+
+<h1 class="text-center m-5"><?= $title ?></h1>
+<div class="container">
+    <?= $content // affichage dynamique des données  ?>
+</div>
+
+
+
+
+
+
+
+
+<?php
+require_once('footer.php');
+?>
---
+++ Views/products/accueil.php
@@ -0,0 +1,20 @@
+<?php var_dump($products) ?>
+
+
+<div class="container p-5">
+    <div class="row justify-content-around">
+        <?php foreach ($products as $product) : ?>
+            <div class="card bg-light mb-3 p-1" style="max-width: 20rem;">
+                <div class="card-header text-center"><?= $product['catTitle'] ?></div>
+                <img src="image/<?= $product['image'] ?>" class="card-img-top" alt="<?= $product['productsTitle'] ?>">
+                <div class="card-body">
+                    <h4 class="card-title"><?= $product['productsTitle'] ?></h4>
+                    <p class="card-text"><?= $product['description'] ?></p>
+                    <p class="card-text text-success"><?= $product['price'] . '€' ?></p>
+                    <a href="/panier/?id=<?= $product['idUser'] ?>" class="btn btn-success m-1">Ajouter au panier</a>
+                    <a href="/detailProduct/?id=<?= $product['idProduct'] ?>" class=" btn btn-info m-1">Détails</a>
+                </div>
+            </div>
+        <?php endforeach ?>
+    </div>
+</div>
